82a94d2f1e1ca770d4d8ecaf7912f65c610f86fd
witre11 <91083439+witre11@users.noreply.github.com>
2023-06-06 14:38:30 -0400

    Delete usager/service directory

--- usager/service/src/main/java/ca/usherbrooke/gegi/server/business/Roles.java
+++
@@ -1,8 +0,0 @@
-package ca.usherbrooke.gegi.server.business;
-
-public final class Roles {
-    public static final String STUDENT = "student";
-    public static final String TEACHER = "teacher";
-    public static final String DEFAULT = "default-roles-master";
-
-}
